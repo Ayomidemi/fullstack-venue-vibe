5ebfb6d0e9066bad3b7de91314de001d292c5b64
Ayomidemi <adenijiayomide13@gmail.com>
2025-01-27 12:46:28 +0100

    project setup

---
+++ .github/pull_request_template.md
@@ -0,0 +1,45 @@
+## Description
+
+<!-- Provide a brief description of the changes made in this PR -->
+- What does this PR do?
+- Why is it necessary?
+
+## Related Issues
+
+<!-- List related issues (e.g., fixes #123) -->
+Fixes #
+
+## Changes Made
+
+<!-- List the changes made in the PR -->
+- [ ] Feature implementation
+- [ ] Bug fix
+- [ ] Refactoring
+- [ ] Documentation updates
+- [ ] Tests added or updated
+- [ ] Other (specify)
+
+## Checklist
+
+<!-- Ensure all checks are complete before submitting -->
+- [ ] I have tested the code and it works as expected
+- [ ] I have added appropriate tests
+- [ ] I have updated relevant documentation
+- [ ] My changes are consistent with the project's coding style
+
+## Screenshots (if applicable)
+
+<!-- Add screenshots to help explain the changes visually -->
+<!-- Example: -->
+<!-- ![Screenshot](url-to-image) -->
+
+## Steps to Test
+
+<!-- Provide step-by-step instructions for testing this PR -->
+1. Pull this branch locally.
+2. Run the following command(s): ...
+3. Verify that ...
+
+## Notes
+
+<!-- Include any additional notes or context -->
---
+++ next.config.js
@@ -0,0 +1,47 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  scope: "/app",
+  disable: process.env.NEXT_PUBLIC_STAGE === "development",
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  fastRefresh: false,
+
+  webpack: (config) => {
+    config.resolve.alias["@components"] = "./src/components";
+    config.resolve.alias["@utils"] = "./src/utils";
+    config.resolve.alias["@global-style"] = "./src/global-style";
+    config.resolve.alias["@actions"] = "./src/actions";
+    config.externals.push({ canvas: "commonjs canvas" });
+    return config;
+  },
+
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "images-cdn.ubuy.co.in",
+      "static.bhphoto.com",
+      "m.media-amazon.com",
+      "api.getquipt.com",
+      "jaybesttech.net",
+      "backmarket.com",
+      "www.backmarket.com",
+      "d2e6ccujb3mkqf.cloudfront.net",
+      "images.alcatelonetouch.us",
+      "www.rvmobileinternet.com",
+      "ss7.vzw.com",
+      "static.swappa.com",
+      "www.computron.com.ec",
+      "aws-obg-image-lb-4.tcl.com",
+      "www.macrosistemas.com",
+      "ssl-product-images.www8-hp.com",
+      "i.ebayimg.com",
+      "storage.googleapis.com",
+      "imagesafriswap.s3.eu-central-1.amazonaws.com",
+    ],
+  },
+});
